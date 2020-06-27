fec35ec0db601b8cf3219e9f9f565df7da5e141e
tanjilul <tanjilulanwar@gmail.com>
2020-06-27 10:54:40 +0600

    server complete

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const cors = require('cors');
+const knex =require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const db= knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smart-brain'
+  }
+});
+
+/*db.select('*').from ('users').then(data=>
+{ console.log(data);
+	});*/
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const database={
+	users:[
+	{
+		id:'123',
+		name:'john',
+		email:'john@gmail.com',
+		password:'cookies',
+		entries:0,
+		joined: new Date()
+	},
+	{
+		id:'456',
+		name:'jack',
+		email:'jack@gmail.com',
+		password:'pasta',
+		entries:0,
+		joined: new Date()
+	}
+
+	]
+}
+
+app.get('/',(req,res)=>{
+	res.send(database.users);
+})
+
+
+app.post('/signin',(req,res)=>{signin.handleSignIn(req,res,db,bcrypt)});
+
+////////////
+app.post('/register',(req,res)=>{register.handleRegister(req,res,db,bcrypt)})
+///////////////////
+
+
+
+
+
+
+app.get('/profile/:id',(req,res)=>{profile.handleProfileGet(req,res,db)})
+
+
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
+
+
+
+ app.listen(3000,()=>{
+ 	console.log("app is running on 3000")
+ })
+ /*
+
+/-->res = this is working
+/signin-->post =success/fail
+/register-->post=user
+/profile/:userId-->Get =user
+/image-->PUT-->user
+
+
+
+ */
